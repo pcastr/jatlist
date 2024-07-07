fab99cf313d58b8b651b42da0588f67b13661c7e
pcastr <lpviancastrodev@gmail.com>
2024-07-07 18:26:11 -0300

    fix(models.py): adiciona valor default para updated_at

--- jatlist_api/jatlist_api/models.py
+++ jatlist_api/jatlist_api/models.py
@@ -18,5 +18,8 @@ class User:
         init=False, server_default=func.now()
     )
     updated_at: Mapped[datetime] = mapped_column(
-        init=False, onupdate=func.now()
+        init=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
     )
