f33f125d219e80087a895c4781c2b616f85d9d5e
pcastr <lpviancastrodev@gmail.com>
2024-07-08 00:37:24 -0300

    refactor(endpoints): atualiza endpoints para usar o banco real

--- jatlist_api/tests/conftest.py
+++ jatlist_api/tests/conftest.py
@@ -1,23 +1,45 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import StaticPool, create_engine
 from sqlalchemy.orm import Session
 
 from jatlist_api.app import app
-from jatlist_api.models import table_registry
+from jatlist_api.database import get_session
+from jatlist_api.models import User, table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
         yield session
 
     table_registry.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Alice', email='alice@example.com', password='secret')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
--- jatlist_api/tests/test_app.py
+++ jatlist_api/tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from jatlist_api.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act (ação)
@@ -26,22 +28,49 @@ def test_create_user(client):
     }
 
 
+def test_create_user_username_exists(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Username already exists'}
+
+
+def test_create_user_email_exists(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'bob',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Email already exists'}
+
+
 def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -72,13 +101,13 @@ def test_update_user_not_found(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_read_user(client):
+def test_read_user(client, user):
     response = client.get('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
+        'username': 'Alice',
+        'email': 'alice@example.com',
         'id': 1,
     }
 
@@ -89,9 +118,10 @@ def test_read_user_not_found(client):
     assert respose.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
 
 
